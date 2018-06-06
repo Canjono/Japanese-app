4d80c19076fe3fb0c0b48cc932a114500fe2e2c2
Thomas Hedlund <thomas.takyon@gmail.com>
2018-06-06 20:30:55 +0200

    backend: Connect to database with Sequlize

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import express_graphql from 'express-graphql'
 import { buildSchema } from 'graphql'
 import cors from 'cors'
+import Sequelize from 'sequelize'
 
 // GraphQL schema.
 const schema = buildSchema(`
@@ -26,7 +27,7 @@ const wordsData = [
         story: 'To kill a zombie you have to CRUSH it'
     },
     {
-        id: '343',
+        id: '12345',
         name: 'Amai',
         translation: 'Sweet',
         grammar: 'Adjective',
@@ -42,6 +43,29 @@ const getWords = () => {
 const root = {
     words: getWords
 }
+
+// Sequlize time!!
+const sequelize = new Sequelize('Vocabulary', 'root', 'root', {
+    host: 'database',
+    dialect: 'mysql',
+    operatorsAliases: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+})
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.')
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err)
+    })
 
 // Create an express server and a GraphQL endpoint.
 const app = express()
