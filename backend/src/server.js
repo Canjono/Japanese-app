914c721244b6891d42ec2b230caa7d0e0bf7e01b
Canjono <michael.hedlund88@gmail.com>
2018-06-09 20:05:42 +0200

    Remove double port variable

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,27 +5,6 @@ import cors from 'cors'
 import db from './database/db'
 import schema from './graphql/schema'
 
-// const wordsData = [
-//     {
-//         id: '11111',
-//         name: 'Korosu',
-//         translation: 'Kill',
-//         grammar: 'Verb',
-//         story: 'To kill a zombie you have to CRUSH it'
-//     },
-//     {
-//         id: '12345',
-//         name: 'Amai',
-//         translation: 'Sweet',
-//         grammar: 'Adjective',
-//         story: 'Bart is sweet when he says, I know who you are, but who AM I?'
-//     }
-// ]
-
-// const getWords = () => {
-//     return wordsData
-// }
-
 // Create an express server and a GraphQL endpoint.
 const app = express()
 
@@ -38,14 +17,6 @@ app.use(
         graphiql: true
     })
 )
-
-const port = 3000
-
-// app.listen(port, () =>
-//     console.log(
-//         `Express GraphQL Server Now Running On localhost:${port}/graphql`
-//     )
-// )
 
 db.authenticate()
     .then(() => {
